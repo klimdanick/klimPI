67337a9a85fd760e1ed0c7748d9f4308bdde838c
klimdanick <klimdanick@gmail.com>
2024-03-06 21:02:25 +0100

    added server endpoint

--- index.js
+++ index.js
@@ -7,5 +7,8 @@ var certificate = fs.readFileSync('certificates/cert.pem', 'utf8');
 var credentials = {key: privateKey, cert: certificate};
 var express = require('express');
 var app = express();
+const port = 8080;
 
-console.log("KlimPI is online!");
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`KlimPI is online! \n at http://localhost:${port}`))
