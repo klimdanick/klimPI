dfb95405e2826e918940a840cffb60accf8a7b16
klimdanick <klimdanick@gmail.com>
2024-03-06 21:39:27 +0100

    added quotes json db

--- index.js
+++ index.js
@@ -5,19 +5,34 @@ const app = express()
 const port = 443
 const exec = require('child_process').exec;
 const execFile = require('child_process').execFile;
+var quotes = readJsonFile("quotes.json")["quotes"];
 
 app.post('/quote', jsonParser, (req, res) => {
-	console.log(req.body);
+	console.log(req.body.auteur + ": " + req.body.quote);
+	quotes.push(req.body);
+	writeJsonFile("quotes.json", {"quotes": quotes);
 	res.send("");
 }) 
 
 app.get('/quote', (req, res) => {
-	res.send("quote");
+	res.send(quotes);
 });
 
 app.get('/quoteBoek', (req, res) => {
 	res.send("<h1>quoteboek</h1>");
 });
+
+function writeJsonFile(file, content) {
+  let jsonData = JSON.stringify(content)
+  fs.writeFileSync(file, jsonData)
+}
+
+function readJsonFile(file) {
+    let bufferData = fs.readFileSync(file)
+    let stData = bufferData.toString()
+    let data = JSON.parse(stData)
+    return data
+}
 
 var https = require('https');
 var fs = require('fs');
