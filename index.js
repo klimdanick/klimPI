205a3b38dfee3ac6c59ec3703f9715b97d9ae088
klimdanick <klimdanick@gmail.com>
2024-03-07 23:55:18 +0100

    fixed intents

--- index.js
+++ index.js
@@ -49,9 +49,9 @@ function readJsonFile(file) {
 }
 
 
-const { Client, Intents } = require('discord.js');
+const { Client, GatewayIntentBits } = require('discord.js');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const client = new Client({ intents: [GatewayIntentBits.FLAGS.GUILDS, GatewayIntentBits.FLAGS.GUILD_MESSAGES] });
 
 client.once('ready', () => {
     console.log('Bot is ready!');
