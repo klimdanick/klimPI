2ed9a66b0fb4cfcebfa23254692d266e8673d94e
klimdanick <klimdanick@gmail.com>
2024-03-08 00:52:39 +0100

    added python bot test

--- bot.py
+++ bot.py
@@ -1,2 +1,32 @@
-while True:
-    print("mila is dom")
+from configparser import ConfigParser
+config = ConfigParser()
+config.read('config.cfg')
+token = config['bot']['token']
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.messages = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    await bot.tree.sync()
+    print(f'We have logged in as {bot.user}')
+    
+
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+@bot.tree.command(name="test",description="test")
+async def slash_command(interaction:discord.Interaction):
+    await interaction.response.send_message("Hello World!")
+
+bot.run(token)
