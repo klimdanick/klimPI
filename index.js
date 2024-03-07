966dac23d5f0d6c23cb7e51d30722378f5233af2
klimdanick <klimdanick@gmail.com>
2024-03-08 00:00:39 +0100

    fixed intents

--- index.js
+++ index.js
@@ -48,6 +48,45 @@ function readJsonFile(file) {
     return data
 }
 
+import { REST, Routes } from 'discord.js';
+
+const commands = [
+  {
+    name: 'ping',
+    description: 'Replies with Pong!',
+  },
+];
+
+const rest = new REST({ version: '10' }).setToken(TOKEN);
+
+try {
+  console.log('Started refreshing application (/) commands.');
+
+  await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
+
+  console.log('Successfully reloaded application (/) commands.');
+} catch (error) {
+  console.error(error);
+}
+
+import { Client, GatewayIntentBits } from 'discord.js';
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+});
+
+client.login(process.env.TOKEN);
+
+/*
 
 const { Client, GatewayIntentBits } = require('discord.js');
 
@@ -67,4 +106,4 @@ client.on('interactionCreate', async interaction => {
     }
 });
 
-client.login(process.env.TOKEN)
+client.login(process.env.TOKEN)*/
