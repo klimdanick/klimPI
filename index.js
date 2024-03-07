34bf88012f0242415559970f8b69650e7ba985ac
klimdanick <klimdanick@gmail.com>
2024-03-07 23:29:15 +0100

    changed intents

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ function readJsonFile(file) {
 
 
 const Discord = require("discord.js")
-const client = new Discord.Client({ intents: ['GUILDS', 'GUILD_MESSAGES']});
+const client = new Discord.Client({ intents: [Discord.GatewayIntentBits.Guilds, Discord.GatewayIntentBits.GuildMessages]});
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
