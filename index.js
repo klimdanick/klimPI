5895e9beffd824e4311d92b92f9d76d85960a723
klimdanick <klimdanick@gmail.com>
2024-04-04 01:49:47 +0200

    initial setup

--- index.js
+++ index.js
@@ -1,71 +1,23 @@
-const express = require('express')
-var bodyParser = require('body-parser')
-var jsonParser = bodyParser.json()
-const app = express()
-require("dotenv").config()
-const port = process.env.PORT
-const exec = require('child_process').exec;
-const execFile = require('child_process').execFile;
-var https = require('https');
+var express = require('express');
+var path = require('path');
 var fs = require('fs');
-var quotes = readJsonFile("quotes.json")["quotes"];
-var metadata = readJsonFile("quotes.json")["metadata"];
-const path = require('path');
 
-var https_options = {
-	key: fs.readFileSync("/certs/private.key"),
-	cert: fs.readFileSync("/certs/certificate.crt"),
-	ca: [fs.readFileSync('/certs/ca_bundle.crt')] 
+const https = require('https');
+const app = express();
+const options = {
+  key: fs.readFileSync('./certs/localhost.decrypted.key'),
+  cert: fs.readFileSync('./certs/localhost.crt')
 };
 
-https.createServer(https_options, app).listen(port)
-
-console.log("KlimPI is online!");
-
-app.post('/quote', jsonParser, (req, res) => {
-	console.log(req.body.auteur + ": " + req.body.quote);
-	quotes.push(req.body);
-	writeJsonFile("quotes.json", {"quotes": quotes, "metadata": metadata});
-	res.send("done!");
-}) 
-
-app.get('/quote', (req, res) => {
-	const dateString = new Date().toLocaleString('nl-NL', { timeZone: 'Europe/Amsterdam' }).slice(0, 10);
-//	let dateString = date.toISOString().slice(0,10);
-	console.log(dateString);
-	if (metadata["dateString"] != dateString) {
-		metadata["dateString"] = dateString;
-		let currentQuote;
-		do {
-			currentQuote = quotes[Math.floor(Math.random() * quotes.length)];
-		} while(currentQuote == metadata["currentQuote"]);
-		metadata["currentQuote"] = currentQuote;
-		writeJsonFile("quotes.json", {"quotes": quotes, "metadata": metadata});
-	}
-	res.send(metadata["currentQuote"]);
+const server = https.createServer(options, app);
+server.listen(443, () => {
+  console.log('HTTPS server running on port 443');
 });
 
-function writeJsonFile(file, content) {
-  let jsonData = JSON.stringify(content)
-  fs.writeFileSync(file, jsonData)
-}
-
-function readJsonFile(file) {
-    let bufferData = fs.readFileSync(file)
-    let stData = bufferData.toString()
-    let data = JSON.parse(stData)
-    return data
-}
-
-exec("python3 bot.py", (error, data, getter) => {
-	if(error){
-		console.log("error",error.message);
-		return;
-	}
-	if(getter){
-		console.log("data",data);
-		return;
-	}
-	console.log("data",data);
-
-});
+app.use(express.static(path.join(__dirname, 'Public')));
+app.get("/server", function (req, res, next) {
+	res.sendFile(path.join(__dirname + '/public/server.html'));
+})
+app.get("*", function (req, res, next) {
+	res.sendFile(path.join(__dirname + '/public/404.html'));
+})
