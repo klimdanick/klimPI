82935688a5fc139b677888dde22016044665f3b2
klimdanick <klimdanick@gmail.com>
2024-04-07 18:30:06 +0200

    fixed shit

--- index.js
+++ index.js
@@ -95,5 +95,5 @@ class Process{
 
 let P = [];
 new Process("Assetto", 0, "/home/steam/assetto/", './acServer');
-new Process("QuoteBot", 1, "~/klimPI/QuoteBot/", './start.sh');
+new Process("QuoteBot", 1, "QuoteBot/", './start.sh');
 console.log(P);
