57ab20eaaa1f6e2fa26d57f5d6ebfdf6346570ef
klimdanick <klimdanick@gmail.com>
2024-04-07 17:37:41 +0200

    added stopping and status

--- public/server.js
+++ public/server.js
@@ -8,44 +8,45 @@ class Process {
 	
 	update() {
 		
-		fetch("https://vps.klimdanick.nl/getStatus").then(response => {return response.text()}).then(Status => {this.Status = Status}).catch(error => {});
-		
-		//this.Status = Status;
-		this.Element = document.createElement("div");
-		this.Element.classList.add("process");
-		let title = document.createElement("div");
-		title.classList.add("process-title");
-		title.innerText = this.Name;
-		this.Element.appendChild(title);
-		let hr = document.createElement("hr");
-		hr.style.width = "100%";
-		hr.style.borderColor = "#299ad0";
-		this.Element.appendChild(hr);
-		let statusSpan = document.createElement("span"); //<hr style="width: 100%; border-color: #299ad0;">
-		statusSpan.classList.add("process-status-label");
-		statusSpan.innerText = "Status | ";//Running";
-		let statusSpan2 = document.createElement("span");
-		statusSpan2.classList.add("process-status");
-		statusSpan2.innerText = this.Status;
-		statusSpan.appendChild(statusSpan2);
-		if (this.Status == "Running") {
-			hr.style.borderColor = "#05d993";
-			statusSpan2.style.color = "#05d993";
-		}
-		this.Element.appendChild(statusSpan);
-		//<span onclick="toggleProcess('Assetto Corsa')" class="process-toggle">Stop</span>
-		let Button = document.createElement("span");
-		Button.classList.add("process-toggle");
-		if (this.Status == "Running") {
-			Button.innerText = "stop";
-			Button.classList.add("Running");
-		}
-		else {
-			Button.innerText = "start";
-			Button.classList.add("Stopped");
-		}
-		this.Element.appendChild(Button);
-		document.documentElement.getElementsByClassName("process-list")[0].appendChild(this.Element);
+		fetch("https://vps.klimdanick.nl/getStatus").then(response => {return response.text()}).then(Status => {
+			this.Status = Status;
+			//this.Status = Status;
+			this.Element = document.createElement("div");
+			this.Element.classList.add("process");
+			let title = document.createElement("div");
+			title.classList.add("process-title");
+			title.innerText = this.Name;
+			this.Element.appendChild(title);
+			let hr = document.createElement("hr");
+			hr.style.width = "100%";
+			hr.style.borderColor = "#299ad0";
+			this.Element.appendChild(hr);
+			let statusSpan = document.createElement("span"); //<hr style="width: 100%; border-color: #299ad0;">
+			statusSpan.classList.add("process-status-label");
+			statusSpan.innerText = "Status | ";//Running";
+			let statusSpan2 = document.createElement("span");
+			statusSpan2.classList.add("process-status");
+			statusSpan2.innerText = this.Status;
+			statusSpan.appendChild(statusSpan2);
+			if (this.Status == "Running") {
+				hr.style.borderColor = "#05d993";
+				statusSpan2.style.color = "#05d993";
+			}
+			this.Element.appendChild(statusSpan);
+			//<span onclick="toggleProcess('Assetto Corsa')" class="process-toggle">Stop</span>
+			let Button = document.createElement("span");
+			Button.classList.add("process-toggle");
+			if (this.Status == "Running") {
+				Button.innerText = "stop";
+				Button.classList.add("Running");
+			}
+			else {
+				Button.innerText = "start";
+				Button.classList.add("Stopped");
+			}
+			this.Element.appendChild(Button);
+			document.documentElement.getElementsByClassName("process-list")[0].appendChild(this.Element);
+		}).catch(error => {});
 	}
 }
 
