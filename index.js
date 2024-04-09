0ba1b13914fac0843cdf5e2682df3ca42f4498a5
klimdanick <klimdanick@gmail.com>
2024-04-09 21:32:20 +0200

    fixed npm start and dev command

--- index.js
+++ index.js
@@ -96,5 +96,5 @@ class Process{
 let P = [];
 new Process("Assetto", 0, "/home/steam/assetto/", './acServer');
 new Process("QuoteBot", 1, "QuoteBot/", './start.sh');
-new Process("QuoteApi", 2, "~/QuoteAPI/", './run.sh');
+new Process("QuoteApi", 2, "~/QuoteAPI/", 'node index.js');
 console.log(P);
