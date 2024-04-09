8aa7fbb1f5ab25089e8e570091f81a3c325c3e01
klimdanick <klimdanick@gmail.com>
2024-04-09 16:05:26 +0200

    f

--- public/server.js
+++ public/server.js
@@ -64,6 +64,7 @@ let Processes = [];
 window.onload = function() {
   Processes.push(new Process("Assetto Corsa Server", 0));
   Processes.push(new Process("QuoteBot", 1));
+  Processes.push(new Process("QuoteApi", 2));
   /*
   Processes.push(new Process("Viking Server", "Stopped"));
   Processes.push(new Process("QuoteApi", "Stopped"));
