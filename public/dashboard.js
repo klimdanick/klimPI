03c6b5bcf5efb1ed130af1161078d1a38979e65e
klimdanick <klimdanick@gmail.com>
2024-04-26 13:43:43 +0200

    g

--- public/dashboard.js
+++ public/dashboard.js
@@ -24,7 +24,7 @@ function nsLoop() {
 	.then(out => {
 		let l = out["payload"]["departures"];
 		document.getElementById("ns").innerHTML="";
-		for (let i = 2; i < l.length; i++) {
+		for (let i = 0; i < Math.min(l.length, 4); i++) {
 			let tijdNu = new Date();
 			let tijdAankomst = new Date(l[i]["actualDateTime"]);
 			let deltaT = (Math.floor((tijdAankomst - tijdNu)/60000))+":"+(Math.floor((tijdAankomst - tijdNu)/1000)%60).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
