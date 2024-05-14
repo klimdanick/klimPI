857cdb3834f8434392b3013f3f5fdc2719ceefdf
klimdanick <klimdanick@gmail.com>
2024-05-14 18:40:58 +0200

    g

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.get("/start/:Id", function (req, res, next) {
 	res.send("started");
 })
 app.get("/getStatus/:Id", function (req, res, next) {
-	console.log(P[req.params.Id]);
+	console.log(req.params.Id + " | " + P.length);
 	res.send(P[req.params.Id].Status);
 })
 app.get("/stop/:Id", function (req, res, next) {
