6c38ece2887253755c78390bd0f61f733bc08041
klimdanick <klimdanick@gmail.com>
2024-05-14 21:35:26 +0200

    fix

--- public/server.js
+++ public/server.js
@@ -14,6 +14,7 @@ ACpros = async (process) => {
 	ACdata = process.tracks = await (await fetch("https://vps.klimdanick.nl/getACdata/"+process.Id)).json();
 	process.tracks = ACdata.tracks;
 	process.cars = ACdata.cars;
+	update(process);
 	return process;
 }
 
