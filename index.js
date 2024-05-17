80620258387d88419ec8804c1b22b619ac1bb809
klimdanick <klimdanick@gmail.com>
2024-05-17 12:41:07 +0200

    fix

--- index.js
+++ index.js
@@ -115,5 +115,5 @@ Process("Assetto", 0, "../acServerManager", './server-manager');
 Process("QuoteBot", 1, "../QuoteBot/", './run.sh');
 Process("E2 Bot", 2, "../E2/", './run.sh');
 Process("x screen", 3, "../torcs/torcs-1.3.7", "./xserver.sh");
-Process("xterm", 4, "../torcs/torcs-1.3.7", "xterm", true, {command: "killall", args: ["xterm"]});
+Process("xterm", 4, "../torcs/torcs-1.3.7/BUILD/bin", "xterm -hold -e ./torcs", true, {command: "killall", args: ["xterm"]});
 setTimeout(() => {console.log(P);},1000);
