318edf74733fe459b5b6435e86c274b4e4881aac
klimdanick <klimdanick@gmail.com>
2024-09-15 21:07:51 +0200

    g

--- public/server.js
+++ public/server.js
@@ -50,6 +50,27 @@ async function update (process) {
 	graph.classList.add("process-graph");
 	process.Element.appendChild(graph);
 
+	process.cpu = [];
+	process.ram = [];
+	process.up = [];
+	process.down = [];
+
+	let date = new Date();
+	let a = Math.random()*2;
+	let b = Math.random()*2;
+	let c = Math.random()*2;
+	let d = Math.random()*2;
+	for (var i = 1; i < 100; i++) {
+		a+= Math.random()-0.5;
+		b+= Math.random()-0.5;
+		c+= Math.random()-0.5;
+		d+= Math.random()-0.5;
+		process.cpu.push({x: new Date(i), y: parseFloat(a)});
+		process.ram.push({x: new Date(i), y: parseFloat(b)});
+		process.up.push({x: new Date(i), y: parseFloat(c)});
+		process.down.push({x: new Date(i), y: parseFloat(d)});
+	}
+
 	let options = {
 		backgroundColor: "transparent",
 		animationEnabled: true,
@@ -64,7 +85,7 @@ async function update (process) {
 		  color: "#d70e48",
 		  xValueType: "dateTime",
 		  xValueFormatString: "DD MMM YY HH:mm",
-		  dataPoints: []
+		  dataPoints: process.cpu
 		},{
 			type: "splineArea", 
 			name: "ram",
