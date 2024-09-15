dea356401eea9b6076756b5ea29bfc3f191a837e
klimdanick <klimdanick@gmail.com>
2024-09-15 21:33:16 +0200

    fix

--- index.js
+++ index.js
@@ -201,7 +201,7 @@ let statsInterval = setInterval(() => {
 		pidusage(P[i].proc.pid, function (err, stats) {
 			if (!stats) return;
 			P[i].stats.cpu.push({x: new Date(), y: parseFloat(stats.cpu)});
-			P[i].stats.ram.push({x: new Date(), y: parseFloat(stats.memory/1280000)});
+			P[i].stats.ram.push({x: new Date(), y: parseFloat(stats.memory/160000000)});
 			P[i].stats.up.push({x: new Date(), y: parseFloat(0)});
 			P[i].stats.down.push({x: new Date(), y: parseFloat(0)});
 			while (P[i].stats.cpu.length > 60) P[i].stats.cpu.shift();
