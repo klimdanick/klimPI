fe908fa35825a0bd994f28c4395ffebd549f41c4
klimdanick <klimdanick@gmail.com>
2024-09-19 23:33:00 +0200

    x

--- index.js
+++ index.js
@@ -144,6 +144,10 @@ app.get("/upload/:fileName/:user", function (req, res, next) {
 	let user = req.params.user;
 	let datetime = new Date().toISOString();
 	res.send({hash, filename, user, datetime});
+
+	let db = JSON.parse(fs.readFileSync("../files/db.json").toString())
+	db["hash"] = {"name": filename, user, datetime}
+	fs.writeFile("../files/db.json", db, (err) => {if (err) throw err;});
 })
 
 Process = (Name, Id, Directory, Command = {"command": "./run.sh", "args": []}, autoRun = true, killcommand = {"command": "term", "args": []}) => {
