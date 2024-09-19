5b41da3de92c98a1d20de8df99645798c47e2822
klimdanick <klimdanick@gmail.com>
2024-09-20 00:17:59 +0200

    x

--- index.js
+++ index.js
@@ -153,6 +153,7 @@ app.get("/upload/:fileName/:user", function (req, res, next) {
 
 app.post("/upload/:fileName/:user", function (req, res, next) {
 	res.send(200)
+	console.log(req.body)
 })
 
 Process = (Name, Id, Directory, Command = {"command": "./run.sh", "args": []}, autoRun = true, killcommand = {"command": "term", "args": []}) => {
