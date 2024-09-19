8b47576969cd9957bddc7a04694378dbc3c11258
klimdanick <klimdanick@gmail.com>
2024-09-19 23:13:12 +0200

    x

--- index.js
+++ index.js
@@ -130,7 +130,7 @@ app.post("/ELEGEN/text/:domain", function (req, res, next) {
 app.get("/download/:fileID", function (req, res, next) {
 	let id = req.params.fileID;
 	let file = JSON.parse(fs.readFileSync("../files/db.json").toString())["files"][id]
-	res.sendFile(path.join(__dirname + `/files/${file.name}`));
+	res.sendFile(path.join(__dirname + `../files/${file.name}`));
 })
 
 Process = (Name, Id, Directory, Command = {"command": "./run.sh", "args": []}, autoRun = true, killcommand = {"command": "term", "args": []}) => {
