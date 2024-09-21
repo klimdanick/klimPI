517edda4449d7bb8a2cf8071be0bc54f99910a79
klimdanick <klimdanick@gmail.com>
2024-09-21 18:24:01 +0200

    x

--- index.js
+++ index.js
@@ -178,7 +178,7 @@ app.get("/fileList", function (req, res, next) {
 
 function checkFile(path) {
 	const fs = require("fs"); // Or `import fs from "fs";` with ESM
-	if (fs.existsSync(path)) {
+	if (!fs.existsSync(path)) {
 		let json = {"files": {}};
 		fs.open(path, 'w', function (err, file) {});
 		fs.writeFileSync(path, JSON.stringify(json));
