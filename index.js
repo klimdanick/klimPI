ff53162f9b317829ddd79531cde58e5caf67236d
klimdanick <klimdanick@gmail.com>
2024-09-27 14:53:35 +0200

    x

--- index.js
+++ index.js
@@ -90,6 +90,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get("/login", function (req, res, next) {
 	res.sendFile(path.join(__dirname + '/public/login.html'));
 })
+app.post("/login", function (req, res, next) {
+	res.sendFile(path.join(__dirname + '/public/login.html'));
+})
 app.get("/dashboard", function (req, res, next) {
 	res.sendFile(path.join(__dirname + '/public/dashboard.html'));
 })
