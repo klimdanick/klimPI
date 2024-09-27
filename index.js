d43ca9c0bbbf18d7127be7b9a66292316bdfc8dd
klimdanick <klimdanick@gmail.com>
2024-09-27 14:46:03 +0200

    x

--- index.js
+++ index.js
@@ -60,8 +60,8 @@ app.use((req, res, next) => {
 	next();
   });
 app.use(bodyParser.urlencoded({extended : true}))
-app.use(bodyParser.raw({inflate:true, limit: '100kb', type: 'application/json'}));
-app.use(bodyParser.raw({inflate:true, limit: '1mb', type: 'text/plain'}))
+// app.use(bodyParser.raw({inflate:true, limit: '100kb', type: 'application/json'}));
+// app.use(bodyParser.raw({inflate:true, limit: '1mb', type: 'text/plain'}))
 app.use(cookieParser())
 app.use((req, res, next) => {
 	if (req.url == "/login") return next();
