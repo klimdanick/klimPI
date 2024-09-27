5d2b3ca52d041b3c58d2e7bf84c5fbfd56752c5b
klimdanick <klimdanick@gmail.com>
2024-09-27 14:59:57 +0200

    x

--- index.js
+++ index.js
@@ -90,6 +90,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get("/login", function (req, res, next) {
 	res.sendFile(path.join(__dirname + '/public/login.html'));
 })
+app.post("/login", function (req, res, next) {
+	res.status(200).redirect("https://vps.klimdanick.nl/server")
+})
 app.get("/dashboard", function (req, res, next) {
 	res.sendFile(path.join(__dirname + '/public/dashboard.html'));
 })
