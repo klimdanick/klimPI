caba1593962689dacb2014d5ad397e5e2d011c41
klimdanick <klimdanick@gmail.com>
2024-09-27 14:56:38 +0200

    x

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ app.use((req, res, next) => {
 	}
 	next();
   });
-//app.use(bodyParser.urlencoded({extended : false}))
+app.use(bodyParser.urlencoded({extended : false}))
 app.use(bodyParser.raw({inflate:true, limit: '100kb', type: 'application/json'}));
 app.use(bodyParser.raw({inflate:true, limit: '1mb', type: 'text/plain'}))
 app.use(cookieParser())
