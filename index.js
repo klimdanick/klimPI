884e76d3b2d1cb4a8b49cb1d635481a1eda2619b
klimdanick <klimdanick@gmail.com>
2024-09-27 14:35:40 +0200

    x

--- index.js
+++ index.js
@@ -81,11 +81,11 @@ app.use((req, res, next) => {
 		return res.status(401).redirect("/login")
 	}
 })
-//app.use(express.static(path.join(__dirname, 'public')));
 app.get("/", function (req, res, next) {
 	if(req.hostname == "vps.klimdanick.nl") res.status(301).redirect("https://vps.klimdanick.nl/server")
 	if(req.hostname == "file.klimdanick.nl") res.sendFile(path.join(__dirname + '/public/file/index.html'));
 })
+app.use(express.static(path.join(__dirname, 'public')));
 app.get("/login", function (req, res, next) {
 	res.sendFile(path.join(__dirname + '/public/login.html'));
 })
