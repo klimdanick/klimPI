056913370d5e7193529b7e8304812e474cf9192a
klimdanick <klimdanick@gmail.com>
2024-10-03 10:32:39 +0200

    x

--- index.js
+++ index.js
@@ -84,7 +84,6 @@ app.use((req, res, next) => {
 			if (db.users[i].username == username && db.users[i].password == password) {
 				let token = "21232f297a57a5a743894a0e4a801fc3";
 				res.cookie('token', token, { maxAge: 900000000, httpOnly: false })
-				res.send(200);
 				console.log(`succesfull login as ${username}!`);
 				return next();
 			}
