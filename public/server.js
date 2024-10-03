55c15a988556cb34289a45bfd559367620b92d39
klimdanick <klimdanick@gmail.com>
2024-10-03 13:19:23 +0200

    x

--- public/server.js
+++ public/server.js
@@ -179,7 +179,7 @@ function toggleProcess(Id) {
 
 async function getLogs(Id) {
 	let response = await (await fetch("https://vps.klimdanick.nl/getLogs/"+Id)).text();
-	console.log(response);
+	document.getElementById("logs").innerText = response;
 }
 
 async function updateGraph(process) {
