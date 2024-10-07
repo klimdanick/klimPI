e3d8269314acc70a45cf2193d35be1f178ff2ff8
klimdanick <klimdanick@gmail.com>
2024-10-07 13:07:31 +0200

    fix file upload

--- index.js
+++ index.js
@@ -37,8 +37,9 @@ server.listen(443, () => {
 const multer = require('multer');
 
 const storage = multer.diskStorage({
-	destination: function (req, file, cb) {
-	  cb(null, "/root/files/"+req.cookies.username+"/");
+	destination: function (req, file, cb) 
+	  let username = getUsernameByToken(req.cookies.token)
+	  cb(null, "/root/files/"+username+"/");
 	},
 	filename: function (req, file, cb) {
 	  cb(null, file.originalname); // Adds a unique timestamp to avoid file overwriting
