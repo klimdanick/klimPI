e13cb6a8b5d1f1539b41c73ec46de2da05a39723
klimdanick <github@klimdanick.nl>
2025-02-10 10:32:38 +0100

    v

--- proxy.js
+++ proxy.js
@@ -1,6 +1,6 @@
 export class Proxy {
     constructor(p) {
-        this.id = p.id || 0;g
+        this.id = p.id || 0;
         this.name = p.name || "New Proxy";
         this.port = p.port || 80;
         this.url = p.url || "/";
