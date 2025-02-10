b173a2ecfa577b7ede82131dbba9f5971aec22c6
klimdanick <github@klimdanick.nl>
2025-02-10 10:53:10 +0100

    hj

--- public/proxy.js
+++ public/proxy.js
@@ -1,7 +1,7 @@
 function ProxyCard(p) {
     let cardLayout = new layout();
     cardLayout.style.backgroundColor = "#ff0000";
-    cardLayout.style.width = "100px";
-    cardLayout.style.height = "100px";
+    cardLayout.htmlEl.width = "100px";
+    cardLayout.htmlEl.height = "10010";
     return cardLayout;
 }
