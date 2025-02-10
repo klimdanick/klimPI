04d1bf4f31e29c70b4e24cab640c87aa706d2464
klimdanick <github@klimdanick.nl>
2025-02-10 12:12:33 +0100

    input

--- public/proxy.js
+++ public/proxy.js
@@ -1,9 +1,30 @@
 function ProxyCard(p) {
     let cardLayout = new Layout();
     let card = new Card();
-    card.appendChild(new Text(p.url));
-    card.appendChild(new Text(p.name));
-    card.appendChild(new Text(p.port));
+    let portInput;
+    card.appendChild(new TextInput(p.name));
+    card.appendChild(new TextInput(p.url));
+    card.appendChild(portInput = new NumberInput(p.port));
+
+    portInput.style.marginTop="100px";
+
     cardLayout.appendChild(card);
     return cardLayout;
+}
+
+class TextInput extends Element {
+    constructor(value) {
+        super("input");
+        this.htmlEl.classList.add("Input");
+        this.htmlEl.value = value;
+    }
+}
+
+class NumberInput extends Element {
+    constructor(value) {
+        super("input");
+        this.htmlEl.classList.add("Input");
+        this.htmlEl.value = value;
+        this.htmlEl.type = "number";
+    }
 }
