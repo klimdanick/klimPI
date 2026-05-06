dae98149534b29dbf125618c83ccd55e57ba3158
klimdanick <github@klimdanick.nl>
2026-05-06 17:33:08 +0200

    added website module

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 import { startProxy } from "./core/proxy/proxy.js"
 import { staticWeb } from "./modules/staticWeb/staticWeb.js"
 
-const website = new staticWeb();
+const website = new staticWeb(8084, "../");
+const adminPanel = new staticWeb(8085, "../frontend");
 
 console.log("Running klimPI");
 
 startProxy(420);
 website.start();
+adminPanel.start();
