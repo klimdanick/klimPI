bca0c333fb537ec2500f43b7ebc08daa6faadc44
klimdanick <github@klimdanick.nl>
2026-05-08 10:59:17 +0200

    cors header fix

--- backend/core/proxy/proxy.js
+++ backend/core/proxy/proxy.js
@@ -68,7 +68,7 @@ const requestHandler = async (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    // res.setHeader('Access-Control-Allow-Credentials', 'true');
 
     if (req.method === 'OPTIONS') {
         res.writeHead(204);
